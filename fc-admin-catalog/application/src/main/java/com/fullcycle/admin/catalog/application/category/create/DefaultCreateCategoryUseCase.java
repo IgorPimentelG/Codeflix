6cd4b429a8b416a7d344948809956731d5996ca6
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2025-03-02 02:42:56 -0300

    :sparkles: feat: add create category use case

---
+++ fc-admin-catalog/application/src/main/java/com/fullcycle/admin/catalog/application/category/create/DefaultCreateCategoryUseCase.java
@@ -0,0 +1,30 @@
+package com.fullcycle.admin.catalog.application.category.create;
+
+import com.fullcycle.admin.catalog.domain.category.Category;
+import com.fullcycle.admin.catalog.domain.category.CategoryGateway;
+import com.fullcycle.admin.catalog.domain.validation.handler.Notification;
+import io.vavr.API;
+import io.vavr.control.Either;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Objects;
+
+@RequiredArgsConstructor
+public class DefaultCreateCategoryUseCase extends CreateCategoryUseCase {
+
+    private final CategoryGateway categoryGateway;
+
+    @Override
+    public Either<Notification, CreateCategoryOutput> execute(CreateCategoryCommand command) {
+        final var notification = Notification.create();
+        final var category = Category.newCategory(command.name(), command.description(), command.isActive());
+        category.validate(notification);
+        return notification.hasError() ? API.Left(notification) : create(category);
+    }
+
+    private Either<Notification, CreateCategoryOutput> create(final Category category) {
+        return API.Try(() -> categoryGateway.create(category))
+          .toEither()
+          .bimap(Notification::create, CreateCategoryOutput::from);
+    }
+}
