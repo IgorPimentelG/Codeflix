7dea296ae9fe7ff31ae579641f7837c06dac1447
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2025-03-02 02:48:15 -0300

    :sparkles: feat: add list category use case

---
+++ fc-admin-catalog/application/src/main/java/com/fullcycle/admin/catalog/application/category/retrieve/list/DefaultListCategoriesUseCase.java
@@ -0,0 +1,18 @@
+package com.fullcycle.admin.catalog.application.category.retrieve.list;
+
+import com.fullcycle.admin.catalog.domain.category.CategoryGateway;
+import com.fullcycle.admin.catalog.domain.category.CategorySearchQuery;
+import com.fullcycle.admin.catalog.domain.pagination.Pagination;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class DefaultListCategoriesUseCase extends ListCategoriesUseCase {
+
+    private CategoryGateway categoryGateway;
+
+    @Override
+    public Pagination<CategoryListOutput> execute(CategorySearchQuery query) {
+        return categoryGateway.findAll(query)
+          .map(CategoryListOutput::from);
+    }
+}
