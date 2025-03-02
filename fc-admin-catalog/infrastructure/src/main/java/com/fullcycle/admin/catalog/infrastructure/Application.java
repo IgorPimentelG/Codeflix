aa15cfe2097eec5d28c67fa71187327497342f6a
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2025-03-02 03:37:32 -0300

    :wrench: chore: add spring framework

--- fc-admin-catalog/infrastructure/src/main/java/com/fullcycle/admin/catalog/infrastructure/Application.java
+++ fc-admin-catalog/infrastructure/src/main/java/com/fullcycle/admin/catalog/infrastructure/Application.java
@@ -1,7 +1,12 @@
 package com.fullcycle.admin.catalog.infrastructure;
 
+import com.fullcycle.admin.catalog.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-
+        SpringApplication.run(WebServerConfig.class, args);
     }
 }
