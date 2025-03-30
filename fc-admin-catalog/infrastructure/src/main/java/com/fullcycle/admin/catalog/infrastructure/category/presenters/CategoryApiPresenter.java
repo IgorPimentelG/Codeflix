a0cd7643f2a6cf91d4b1486ad77aed185c5ab9da
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2025-03-30 02:48:54 -0300

    :sparkles: feat: add category presenter

---
+++ fc-admin-catalog/infrastructure/src/main/java/com/fullcycle/admin/catalog/infrastructure/category/presenters/CategoryApiPresenter.java
@@ -0,0 +1,31 @@
+package com.fullcycle.admin.catalog.infrastructure.category.presenters;
+
+import com.fullcycle.admin.catalog.application.category.retrieve.get.CategoryOutput;
+import com.fullcycle.admin.catalog.application.category.retrieve.list.CategoryListOutput;
+import com.fullcycle.admin.catalog.infrastructure.category.models.CategoryResponse;
+import com.fullcycle.admin.catalog.infrastructure.category.models.CategoryListResponse;
+
+public interface CategoryApiPresenter {
+
+    static CategoryResponse present(final CategoryOutput output) {
+        return new CategoryResponse(
+          output.id().getValue().toString(),
+          output.name(),
+          output.description(),
+          output.isActive(),
+          output.createdAt(),
+          output.updatedAt(),
+          output.deletedAt()
+        );
+    }
+
+    static CategoryListResponse present(final CategoryListOutput output) {
+        return new CategoryListResponse(
+          output.id().getValue().toString(),
+          output.name(),
+          output.description(),
+          output.isActive(),
+          output.createdAt()
+        );
+    }
+}
