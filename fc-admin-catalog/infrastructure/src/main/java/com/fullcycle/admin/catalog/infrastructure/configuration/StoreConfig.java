f2e053920266dccb6b41fc1a27b63fc11af81c03
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2025-05-25 01:11:12 -0300

    :sparkles: feat: add bean to storage service

---
+++ fc-admin-catalog/infrastructure/src/main/java/com/fullcycle/admin/catalog/infrastructure/configuration/StoreConfig.java
@@ -0,0 +1,26 @@
+package com.fullcycle.admin.catalog.infrastructure.configuration;
+
+import com.fullcycle.admin.catalog.infrastructure.configuration.properties.GCStorageProperties;
+import com.fullcycle.admin.catalog.infrastructure.service.StorageService;
+import com.fullcycle.admin.catalog.infrastructure.service.impl.GCStorageService;
+import com.fullcycle.admin.catalog.infrastructure.service.local.InMemoryStorageService;
+import com.google.cloud.storage.Storage;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+public class StoreConfig {
+
+	@Bean(name = "storageService")
+	@Profile({"development", "production"})
+	public StorageService gcStorageService(final GCStorageProperties properties, final Storage storage) {
+		return new GCStorageService(properties.getBucket(), storage);
+	}
+
+	@Bean(name = "storageService")
+	@Profile("test")
+	public StorageService inMemoryStorageService() {
+		return new InMemoryStorageService();
+	}
+}
